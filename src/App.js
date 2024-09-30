d9015999bb3f19f72b3fdec50bda528a89448424
rezafaghih <royalgaming.work@gmail.com>
2024-10-01 03:15:42 +0330

    new episode ( useContext ) finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, createContext } from 'react';
+import {Auth} from "./components/Auth"
+
+export const ThemeContext = createContext("light")
 
 function App() {
   const [number, setNumber] = useState(0)
   const [name, setName] = useState('')
   const [isAppReady, setAppReadey] = useState(false)
-
+  const [theme , setTheme] = useState('dark')
+  const input = useRef();
   const changeName =()=>{
     setName("reza")
   }
-
   useEffect(()=>{
     if (!isAppReady){
       changeName()
@@ -19,12 +22,16 @@ function App() {
 
 
   return (
-    <div className = "container">
-      <h1 className='main-text'>Hello {name}</h1>
-      <span style={{fontSize:"20px", backgroundColor:"pink"}}>{number}</span>
-
-      <button onClick={()=>{setNumber(number-1)}}>click on me</button>
-    </div>
+    <ThemeContext.Provider value = {{theme, setTheme}}>
+      <div className = "container">
+        <Auth num = {number}/>
+        <h1 className='main-text'>Hello {name}</h1>
+        <span style={{fontSize:"20px", backgroundColor:"pink"}}>{number}</span>
+        <input type='text' ref={input}/>
+        <span>website theme is {theme}</span>
+        <button onClick={()=>{setNumber(number+1)}}>click on me</button>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
