1f9506da6a131174cad9d09f63a6d0047117d26b
rezafaghih <royalgaming.work@gmail.com>
2024-09-30 19:36:02 +0330

    first episodes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0)
+  const [name, setName] = useState('')
+  const [isAppReady, setAppReadey] = useState(false)
+
+  const changeName =()=>{
+    setName("reza")
+  }
+
+  useEffect(()=>{
+    if (!isAppReady){
+      changeName()
+      setAppReadey(true)
+    }
+  }, [isAppReady])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container">
+      <h1 className='main-text'>Hello {name}</h1>
+      <span style={{fontSize:"20px", backgroundColor:"pink"}}>{number}</span>
+
+      <button onClick={()=>{setNumber(number-1)}}>click on me</button>
     </div>
   );
 }
